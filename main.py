f589022a9777dfb8f97a1c7371e5947106252fb2
Damien Labbe <damien.labbe@fms-ea.com>
2026-07-23 11:26:44 +0200

    ajout de la fonction preparations_battle

--- main.py
+++ main.py
@@ -10,4 +10,31 @@ def input_playeurs() -> int:
         if user in liste_number_str:
             return int(user)
 
+def preparations_battle() -> list[BatlleshipGame]:
+    nb_playeur = input_playeurs()
+    list_playeur: list[str] = []
+    for _ in range(nb_playeur):
+        list_playeur.append(input("entre un nom"))
+
+    for i in range(2 - nb_playeur):
+        list_playeur.append(f"\n{i}")
+
+    ships_names: list[str] = ["Jupiter", "Zeus", "Aphrodite", "Vénus", "Marth", "Lucina", "Mars", "Arès", "Hades",
+                              "Pluton", "Anubis", "Ra", "Mercure", "Hermès", "Athéna", "Minerve", "Minerva", "Corrin",
+                              "Byleth", "Bastet", "Thor", "Odin", "Grima", "Corren", "Casper", "Pill", "Halt", "Treaty",
+                              "Oblige", "Seth", "Théménos", "Cyrus", "Hephaïstos", "Cupidon", "Zorro", "Lefantôme"]
+    playeurs: list[BatlleshipGame] = []
+
+    for playeur in range(2):
+        ships_playeur: list[str] = []
+        for _ in range(5):
+            ship = choice(ships_names)
+            ships_playeur.append(ship)
+            ships_names.remove(ship)
+
+        playeurs.append(BatlleshipGame(list_playeur[playeur], ships_playeur, list_playeur[playeur - 1]))
+
+    return playeurs
+
+
 
