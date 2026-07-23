7e3f9bddfc460d17878cf7509ffec8d8ce38023a
Damien Labbe <damien.labbe@fms-ea.com>
2026-07-23 11:25:50 +0200

    ajout de la fonction input_playeurs

--- main.py
+++ main.py
@@ -2,4 +2,12 @@ from classe_jeu import BatlleshipGame
 from secrets import choice
 
 
+def input_playeurs() -> int:
+    user = ""
+    liste_number_str = [str(i) for i in range(3)]
+    while isinstance(user, str):
+        user = input("Veillez entre le nombre de joueur (entre 0 et 2 inclus")
+        if user in liste_number_str:
+            return int(user)
+
 
